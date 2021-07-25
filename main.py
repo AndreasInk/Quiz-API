16011110e835091f75aa2ef56879fcb002a15d0f
Andreas Ink <andreasink17@gmail.com>
2021-07-25 00:29:34 -0400

    Moved files

---
+++ main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+
+#!pip install  -U transformers==3.0.0
+
+#!python -m  nltk.downloader punkt
+
+# !git clone https://github.com/patil-suraj/question_generation.git
+
+
+
+
+#!pip install flask-ngrok
+
+from flask import Flask, request, Response
+import json
+from pipelines import pipeline
+import streamlit as st
+
+#run_with_ngrok(app)   #starts ngrok when the app is run
+nlp = pipeline("question-generation")
+
+def quizify():
+  
+
+  textin = st.text_input("Input Text")
+
+  quiz = nlp(textin)
+
+  status = {}
+  status['quiz'] = quiz
+
+  statusjson = json.dumps(status)
+
+  resp = Response(statusjson, status=200, mimetype='application/json')
+
+  st.text(resp)
+
+
+
+quizify()
