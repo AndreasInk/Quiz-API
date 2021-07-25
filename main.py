1380acf42b83c46e0fb3a0824bf26f970d5fe51f
Andreas Ink <andreasink17@gmail.com>
2021-07-25 05:06:25 -0400

    Experimenting

--- main.py
+++ main.py
@@ -17,24 +17,26 @@ import json
 from pipelines import pipeline
 import streamlit as st
 import nltk
+import requests
 #run_with_ngrok(app)   #starts ngrok when the app is run
 nlp = pipeline("question-generation")
 nltk.download('punkt')
 def quizify():
   
 
-  textin = st.text_input("Input Text")
+  textin = st.text_input("")
+  if textin != "":
+    quiz = nlp(textin)
 
-  quiz = nlp(textin)
+    status = {}
+    status['quiz'] = quiz
 
-  status = {}
-  status['quiz'] = quiz
+    statusjson = json.dumps(status)
 
-  statusjson = json.dumps(status)
+    resp = Response(statusjson, status=200, mimetype='application/json')
 
-  resp = Response(statusjson, status=200, mimetype='application/json')
-
-  st.text(quiz)
+    st.text(quiz)
+    return quiz
 
 
 
